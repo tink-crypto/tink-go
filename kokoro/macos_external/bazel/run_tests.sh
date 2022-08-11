55505f303a647fa97ae4446b5abdd0f0073b0d43
Moreno Ambrosin <ambrosin@google.com>
2022-08-11 02:50:11 -0700

    Add Kokoro test scripts for MacOS
    PiperOrigin-RevId: 466905251
    Change-Id: I14fc983ef3ed5fbe2dfd36b5be6be6ce71cbe3b9

---
+++ kokoro/macos_external/bazel/run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Copyright 2022 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+################################################################################
+
+set -euo pipefail
+
+if [[ -n "${KOKORO_ROOT:-}" ]]; then
+  cd "${KOKORO_ARTIFACTS_DIR}/git/tink_go"
+  use_bazel.sh "$(cat .bazelversion)"
+fi
+
+# Sourcing required to update callers environment.
+source ./kokoro/testutils/install_go.sh
+
+echo "Using go binary from $(which go): $(go version)"
+
+./kokoro/testutils/check_go_generated_files_up_to_date.sh .
+./kokoro/testutils/run_bazel_tests.sh .
