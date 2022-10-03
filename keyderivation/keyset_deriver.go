cf38fb44ea2c7ece93072cfc672a8eec8aba63e3
Cindy Lin <cinlin@google.com>
2022-10-03 11:55:50 -0700

    Implement PRF-Based Deriver key manager.
    PiperOrigin-RevId: 478566206
    Change-Id: I0a604051d01f24aeb9ee2e0534d907e6ab3b580d

--- keyderivation/keyset_deriver.go
+++ keyderivation/keyset_deriver.go
@@ -19,6 +19,9 @@
 package keyderivation
 
 import (
+	"fmt"
+
+	"github.com/tink-crypto/tink-go/core/registry"
 	"github.com/tink-crypto/tink-go/keyset"
 )
 
@@ -32,4 +35,10 @@ import (
 // salt which the attacker attacks.
 type KeysetDeriver interface {
 	DeriveKeyset(salt []byte) (*keyset.Handle, error)
+}
+
+func init() {
+	if err := registry.RegisterKeyManager(new(prfBasedDeriverKeyManager)); err != nil {
+		panic(fmt.Sprintf("keyderivation.init() failed: %v", err))
+	}
 }
