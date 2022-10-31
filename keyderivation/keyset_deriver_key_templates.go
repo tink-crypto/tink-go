ad92361c6b0b6612b18ce7def53a808ac73e86bd
Charles Lee <ckl@google.com>
2022-10-31 14:43:07 -0700

    Handle proto.Marshal() error in CreatePRFBasedKeyTemplate().
    PiperOrigin-RevId: 485153040
    Change-Id: I6ac4f0703ba2c01c18707aa5ccc252f346658494

--- keyderivation/keyset_deriver_key_templates.go
+++ keyderivation/keyset_deriver_key_templates.go
@@ -17,6 +17,8 @@
 package keyderivation
 
 import (
+	"fmt"
+
 	"google.golang.org/protobuf/proto"
 	"github.com/tink-crypto/tink-go/keyset"
 	prfderpb "github.com/tink-crypto/tink-go/proto/prf_based_deriver_go_proto"
@@ -33,7 +35,10 @@ func CreatePRFBasedKeyTemplate(prfKeyTemplate, derivedKeyTemplate *tinkpb.KeyTem
 			DerivedKeyTemplate: derivedKeyTemplate,
 		},
 	}
-	serializedFormat, _ := proto.Marshal(keyFormat)
+	serializedFormat, err := proto.Marshal(keyFormat)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal key format: %s", err)
+	}
 	template := &tinkpb.KeyTemplate{
 		TypeUrl:          prfBasedDeriverTypeURL,
 		OutputPrefixType: derivedKeyTemplate.GetOutputPrefixType(),
