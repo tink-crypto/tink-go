de043db8029ec06895ba74eaa8b812f13e47ad74
Moreno Ambrosin <ambrosin@google.com>
2024-07-17 06:58:45 -0700

    Remove unneeded method from testConfig
    PiperOrigin-RevId: 653219409
    Change-Id: If9a20ca4c92d07197bc8e497800917442602f5fd

--- keyset/handle_test.go
+++ keyset/handle_test.go
@@ -579,10 +579,6 @@ func TestPrimitivesWithRegistry(t *testing.T) {
 
 type testConfig struct{}
 
-func (c *testConfig) PrimitiveFromKeyData(_ *tinkpb.KeyData, _ internalapi.Token) (any, error) {
-	return testPrimitive{}, nil
-}
-
 func (c *testConfig) PrimitiveFromKey(_ key.Key, _ internalapi.Token) (any, error) {
 	return testPrimitive{}, nil
 }
