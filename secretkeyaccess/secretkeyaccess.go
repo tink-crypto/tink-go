473316056cc54935fcf83b5082b635d9996c5b07
Moreno Ambrosin <ambrosin@google.com>
2024-08-16 15:20:45 -0700

    Add the Bytes type to the secretkeyaccess package
    This is a convenient type that ensures immutability of the wrapped secret key bytes and access to the wrapped bytes only with a valid access token.
    
    Internally at Google, this struct is used in conjunction with the build system to restrict access to functions that return secret key material.
    
    #tinkApiChange
    
    PiperOrigin-RevId: 663887280
    Change-Id: Ifde63241a0ef94516958d609acfe96cff605ac37

--- secretkeyaccess/secretkeyaccess.go
+++ secretkeyaccess/secretkeyaccess.go
@@ -12,32 +12,42 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package secretkeyaccess provides a function to validate secret key access tokens.
+// Package secretkeyaccess provides utilities for APIs that return secret key
+// material and need to validate secret key access tokens.
 //
-// This package is intended for use in APIs that return secret key material to
+// This package is intended for use in APIs that return secret key bytes to
 // avoid taking a direct dependency on the insecuresecretkeyaccess package.
-// Consumers of secret key material should use the insecuresecretkeyaccess
+// Consumers of secret key bytes should use the insecuresecretkeyaccess
 // package directly.
 //
-// For example, an API that returns secret key material could be defined as:
+// TODO: b/354103265 - Move this to an example test.
 //
-//	func MyFunction(token any) (secretKey []byte, err error) {
-//		if err := secretkeyaccess.Validate(token); err != nil {
-//			return nil, err
-//		}
+// For example, an API that returns secret key bytes could simply do:
+//
+//	func MyFunction() (secretKey Bytes, err error) {
 //		// ...
 //	}
 //
-// The API is used as follows:
+// Then to access the wrapped bytes, the caller would do:
 //
-//	secretKey, err := MyFunction(insecuresecretkeyaccess.Token{})
+//	secretKey, err := MyFunction()
+//	if err != nil {
+//		return err
+//	}
+//	secretKeyMaterial, err := secretKey.Data(insecuresecretkeyaccess.Token{})
+//	if err != nil {
+//		return err
+//	}
 //	// ...
 //
-// Internally at Google, this package is used in conjunction with the build
-// system to restrict access to functions that return secret key material.
+// This package and build restrictions on insecuresecretkeyaccess may be used
+// together to restrict access to secret key bytes.
 package secretkeyaccess
 
 import (
+	"bytes"
+	"crypto/rand"
+	"crypto/subtle"
 	"fmt"
 
 	"github.com/tink-crypto/tink-go/v2/insecuresecretkeyaccess"
@@ -45,14 +55,68 @@ import (
 
 // Validate validates a secret key access token.
 //
-// This function should be used by APIs that return secret key material to
+// This function should be used by APIs that return secret key bytes to
 // validate that the caller has a valid token.
 //
-// Internally at Google, this function is used in conjunction with the build
-// system to restrict access to functions that return secret key material.
+// This function and build restrictions on insecuresecretkeyaccess may be used
+// together to restrict access to secret key bytes.
 func Validate(token any) error {
 	if _, ok := token.(insecuresecretkeyaccess.Token); !ok {
 		return fmt.Errorf("secret key access token is not of type insecuresecretkeyaccess.Token, got %T", token)
 	}
 	return nil
+}
+
+// Bytes is a wrapper around []byte that requires a secret key access token to
+// access a copy of the data.
+//
+// This type ensures immutability of the wrapped bytes.
+//
+// This type and build restrictions on insecuresecretkeyaccess may be used
+// together to restrict access to secret key bytes.
+type Bytes struct {
+	data []byte
+}
+
+// NewBytes creates a new Bytes with size bytes of cryptographically strong
+// random data.
+func NewBytes(size uint32) (*Bytes, error) {
+	b := &Bytes{data: make([]byte, size)}
+	if _, err := rand.Read(b.data); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+// NewBytesFromData creates a new Bytes populated with data.
+//
+// This function make a copy of the data. Returns an error if token is not an
+// [insecuresecretkeyaccess.Token].
+func NewBytesFromData(data []byte, token any) (*Bytes, error) {
+	if err := Validate(token); err != nil {
+		return nil, err
+	}
+	return &Bytes{data: bytes.Clone(data)}, nil
+}
+
+// Data returns a copy of the wrapped bytes.
+//
+// Returns an error if token is not an [insecuresecretkeyaccess.Token].
+func (b *Bytes) Data(token any) ([]byte, error) {
+	if err := Validate(token); err != nil {
+		return nil, err
+	}
+	return bytes.Clone(b.data), nil
+}
+
+// Len returns the size of the wrapped bytes.
+func (b *Bytes) Len() int { return len(b.data) }
+
+// Equals returns true if the two Bytes objects are equal.
+//
+// The comparison is done in constant time. The time taken is a function of the
+// length of the wrapped bytes and is independent of the contents. If the two
+// wrapped slices are of different lengths, the function returns immediately.
+func (b *Bytes) Equals(other *Bytes) bool {
+	return subtle.ConstantTimeCompare(b.data, other.data) == 1
 }
--- secretkeyaccess/secretkeyaccess_test.go
+++ secretkeyaccess/secretkeyaccess_test.go
@@ -15,6 +15,7 @@
 package secretkeyaccess_test
 
 import (
+	"bytes"
 	"testing"
 
 	"github.com/tink-crypto/tink-go/v2/insecuresecretkeyaccess"
@@ -27,17 +28,178 @@ func TestValidateWithValidToken(t *testing.T) {
 	}
 }
 
-func TestValidateWithInvalidTokenReturnsError(t *testing.T) {
-	if err := secretkeyaccess.Validate(nil); err == nil {
-		t.Errorf("secretkeyaccess.Validate(nil) = nil, want error")
+func TestInvalidTokenReturnsError(t *testing.T) {
+	for _, tc := range []struct {
+		name  string
+		token any
+	}{
+		{
+			name:  "nil",
+			token: nil,
+		},
+		{
+			name:  "int",
+			token: 42,
+		},
+		{
+			name:  "string",
+			token: "token",
+		},
+		{
+			name:  "reference to insecuresecretkeyaccess.Token",
+			token: &insecuresecretkeyaccess.Token{},
+		},
+	} {
+		t.Run("Validate_"+tc.name, func(t *testing.T) {
+			if err := secretkeyaccess.Validate(tc.token); err == nil {
+				t.Errorf("secretkeyaccess.Validate(%v) = nil, want error", tc.token)
+			}
+		})
+
+		data := []byte("secret key material")
+		t.Run("NewBytesFromData_"+tc.name, func(t *testing.T) {
+			if _, err := secretkeyaccess.NewBytesFromData(data, tc.token); err == nil {
+				t.Errorf("secretkeyaccess.NewBytesFromData(%v, %v) = nil, want error", data, tc.token)
+			}
+		})
+
+		t.Run("BytesData_"+tc.name, func(t *testing.T) {
+			keyMaterial, err := secretkeyaccess.NewBytes(16)
+			if err != nil {
+				t.Fatalf("secretkeyaccess.NewBytes(16) = %v, want nil", err)
+			}
+			if _, err := keyMaterial.Data(tc.token); err == nil {
+				t.Errorf("keyMaterial.Data(%v) = nil, want error", tc.token)
+			}
+		})
 	}
-	if err := secretkeyaccess.Validate(42); err == nil {
-		t.Errorf("secretkeyaccess.Validate(42) = nil, want error")
+}
+
+func TestNewBytesLen(t *testing.T) {
+	for _, size := range []uint32{0, 1, 16, 1024} {
+		keyMaterial, err := secretkeyaccess.NewBytes(size)
+		if err != nil {
+			t.Fatalf("secretkeyaccess.NewBytes(%v) = %v, want nil", size, err)
+		}
+		if got, want := keyMaterial.Len(), int(size); got != want {
+			t.Errorf("keyMaterial.Len() = %v, want %v", got, want)
+		}
 	}
-	if err := secretkeyaccess.Validate("token"); err == nil {
-		t.Errorf("secretkeyaccess.Validate(\"token\") = nil, want error")
+}
+
+func TestNewBytesFromDataLen(t *testing.T) {
+	data := []byte("secret key material")
+	keyMaterial, err := secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
 	}
-	if err := secretkeyaccess.Validate(&insecuresecretkeyaccess.Token{}); err == nil {
-		t.Errorf("secretkeyaccess.Validate(&insecuresecretkeyaccess.Token{}) = nil, want error")
+	if got, want := keyMaterial.Len(), len(data); got != want {
+		t.Errorf("keyMaterial.Len() = %v, want %v", got, want)
+	}
+}
+
+func TestBytesWithNilBytesHasZeroLen(t *testing.T) {
+	keyMaterial, err := secretkeyaccess.NewBytesFromData(nil, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(nil, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	if got, want := keyMaterial.Len(), 0; got != want {
+		t.Errorf("keyMaterial.Len() = %v, want %v", got, want)
+	}
+}
+
+func TestBytesBytesWithValidTokenReturnsData(t *testing.T) {
+	expected := []byte("secret key material")
+	keyMaterial, err := secretkeyaccess.NewBytesFromData(expected, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(expected, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	got, err := keyMaterial.Data(insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("keyMaterial.Data(insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	if !bytes.Equal(got, expected) {
+		t.Errorf("bytes.Equal(got, expected) = false, want true")
+	}
+}
+
+func TestBytesEquals(t *testing.T) {
+	data := []byte("secret key material")
+	keyMaterial, err := secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	otherBytes, err := secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	if !keyMaterial.Equals(otherBytes) {
+		t.Errorf("keyMaterial.Equals(otherBytes) = false, want true")
+	}
+
+	differentBytes, err := secretkeyaccess.NewBytesFromData([]byte("different secret key material"), insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(data, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	if differentBytes.Equals(keyMaterial) {
+		t.Errorf("differentBytes.Equals(keyMaterial) = true, want false")
+	}
+}
+
+func TestBytesEqualsEmpty(t *testing.T) {
+	nilSecretBytes, err := secretkeyaccess.NewBytesFromData(nil, insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData(nil, insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	emptySecretBytes, err := secretkeyaccess.NewBytesFromData([]byte(""), insecuresecretkeyaccess.Token{})
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytesFromData([]byte(\"\"), insecuresecretkeyaccess.Token{}) = %v, want nil", err)
+	}
+	randomEmptyBytes, err := secretkeyaccess.NewBytes(0)
+	if err != nil {
+		t.Fatalf("secretkeyaccess.NewBytes(0) = %v, want nil", err)
+	}
+	testCases := []struct {
+		name        string
+		firstBytes  *secretkeyaccess.Bytes
+		secondBytes *secretkeyaccess.Bytes
+	}{
+		{
+			name:        "nil vs nil",
+			firstBytes:  nilSecretBytes,
+			secondBytes: nilSecretBytes,
+		},
+		{
+			name:        "empty vs empty",
+			firstBytes:  emptySecretBytes,
+			secondBytes: emptySecretBytes,
+		},
+		{
+			name:        "random empty vs random empty",
+			firstBytes:  randomEmptyBytes,
+			secondBytes: randomEmptyBytes,
+		},
+		{
+			name:        "nil vs empty",
+			firstBytes:  nilSecretBytes,
+			secondBytes: emptySecretBytes,
+		},
+		{
+			name:        "nil vs random empty",
+			firstBytes:  nilSecretBytes,
+			secondBytes: randomEmptyBytes,
+		},
+		{
+			name:        "empty vs random empty",
+			firstBytes:  emptySecretBytes,
+			secondBytes: randomEmptyBytes,
+		},
+	}
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			if !testCase.firstBytes.Equals(testCase.secondBytes) {
+				t.Errorf("firstBytes.Equals(secondBytes) = false, want true")
+			}
+		})
 	}
 }
