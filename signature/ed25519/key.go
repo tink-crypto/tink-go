c2103562243f7d93891b61d04230b8b6f72c85e0
Moreno Ambrosin <ambrosin@google.com>
2024-09-09 23:55:51 -0700

    Add ED25519 key parameters definition to Tink Go
    #tinkApiChange
    
    PiperOrigin-RevId: 672829134
    Change-Id: I82726cd05557fba105c99e5788dcc14ca806d8cd

---
+++ signature/ed25519/key.go
@@ -0,0 +1,89 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package ed25519 provides ED25519 keys and parameters definitions.
+package ed25519
+
+import (
+	"fmt"
+
+	"github.com/tink-crypto/tink-go/v2/key"
+)
+
+// Variant is the prefix variant of an ED25519 key.
+//
+// It describes the format of the signature. For ED25519, there are four options:
+//
+//   - TINK: prepends '0x01<big endian key id>' to the signature.
+//   - CRUNCHY: prepends '0x00<big endian key id>' to the signature.
+//   - LEGACY: appends a 0-byte to the input message before computing the
+//     signature, then prepends '0x00<big endian key id>' to the signature.
+//   - NO_PREFIX: adds no prefix to the signature.
+type Variant int
+
+const (
+	// VariantUnknown is the default value of Variant.
+	VariantUnknown Variant = iota
+	// VariantTink prefixes '0x01<big endian key id>' to the signature.
+	VariantTink
+	// VariantCrunchy prefixes '0x00<big endian key id>' to the signature.
+	VariantCrunchy
+	// VariantLegacy appends a 0-byte to input message BEFORE computing the signature,
+	// signature, then prepends '0x00<big endian key id>' to signature.
+	VariantLegacy
+	// VariantNoPrefix does not prefix the signature with the key id.
+	VariantNoPrefix
+)
+
+func (variant Variant) String() string {
+	switch variant {
+	case VariantTink:
+		return "TINK"
+	case VariantCrunchy:
+		return "CRUNCHY"
+	case VariantLegacy:
+		return "LEGACY"
+	case VariantNoPrefix:
+		return "NO_PREFIX"
+	default:
+		return "UNKNOWN"
+	}
+}
+
+// Parameters represents the parameters of an ED25519 key.
+type Parameters struct {
+	variant Variant
+}
+
+var _ key.Parameters = (*Parameters)(nil)
+
+// NewParameters creates a new Parameters.
+func NewParameters(variant Variant) (Parameters, error) {
+	if variant == VariantUnknown {
+		return Parameters{}, fmt.Errorf("ed25519.NewParameters: variant must not be %v", VariantUnknown)
+	}
+	return Parameters{variant: variant}, nil
+}
+
+// HasIDRequirement returns true if the key has an ID requirement.
+func (p *Parameters) HasIDRequirement() bool { return p.variant != VariantNoPrefix }
+
+// Equals returns true if this parameters object is equal to other.
+func (p *Parameters) Equals(other key.Parameters) bool {
+	if p == other {
+		return true
+	}
+	then, ok := other.(*Parameters)
+	return ok && p.variant == then.variant
+}
---
+++ signature/ed25519/key_test.go
@@ -0,0 +1,165 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ed25519_test
+
+import (
+	"testing"
+
+	"github.com/tink-crypto/tink-go/v2/signature/ed25519"
+)
+
+func TestNewParameters(t *testing.T) {
+	for _, tc := range []struct {
+		name    string
+		variant ed25519.Variant
+	}{
+		{
+			name:    "tink",
+			variant: ed25519.VariantTink,
+		},
+		{
+			name:    "legacy",
+			variant: ed25519.VariantLegacy,
+		},
+		{
+			name:    "crunchy",
+			variant: ed25519.VariantCrunchy,
+		},
+		{
+			name:    "no prefix",
+			variant: ed25519.VariantNoPrefix,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			_, err := ed25519.NewParameters(tc.variant)
+			if err != nil {
+				t.Errorf("ed25519.NewParameters(%v) err = %v, want nil", tc.variant, err)
+			}
+		})
+	}
+	t.Run("unknown", func(t *testing.T) {
+		if _, err := ed25519.NewParameters(ed25519.VariantUnknown); err == nil {
+			t.Errorf("ed25519.NewParameters(%v) err = nil, want error", ed25519.VariantUnknown)
+		}
+	})
+}
+
+func TestParametersHasIDRequirement(t *testing.T) {
+	for _, tc := range []struct {
+		name    string
+		variant ed25519.Variant
+		want    bool
+	}{
+		{
+			name:    "tink",
+			variant: ed25519.VariantTink,
+			want:    true,
+		},
+		{
+			name:    "legacy",
+			variant: ed25519.VariantLegacy,
+			want:    true,
+		},
+		{
+			name:    "crunchy",
+			variant: ed25519.VariantCrunchy,
+			want:    true,
+		},
+		{
+			name:    "no prefix",
+			variant: ed25519.VariantNoPrefix,
+			want:    false,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			params, err := ed25519.NewParameters(tc.variant)
+			if err != nil {
+				t.Fatalf("ed25519.NewParameters(%v) err = %v, want nil", tc.variant, err)
+			}
+			if got := params.HasIDRequirement(); got != tc.want {
+				t.Errorf("params.HasIDRequirement() = %v, want %v", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestParametersEquals(t *testing.T) {
+	tinkVariant, err := ed25519.NewParameters(ed25519.VariantTink)
+	if err != nil {
+		t.Fatalf("ed25519.NewParameters(%v) err = %v, want nil", ed25519.VariantTink, err)
+	}
+	legacyVariant, err := ed25519.NewParameters(ed25519.VariantLegacy)
+	if err != nil {
+		t.Fatalf("ed25519.NewParameters(%v) err = %v, want	 nil", ed25519.VariantLegacy, err)
+	}
+	crunchyVariant, err := ed25519.NewParameters(ed25519.VariantCrunchy)
+	if err != nil {
+		t.Fatalf("ed25519.NewParameters(%v) err = %v, want nil", ed25519.VariantCrunchy, err)
+	}
+	noPrefixVariant, err := ed25519.NewParameters(ed25519.VariantNoPrefix)
+	if err != nil {
+		t.Fatalf("ed25519.NewParameters(%v) err = %v, want	 nil", ed25519.VariantNoPrefix, err)
+	}
+
+	for _, params := range []ed25519.Parameters{tinkVariant, legacyVariant, crunchyVariant, noPrefixVariant} {
+		if !params.Equals(&params) {
+			t.Errorf("params.Equals(params) = false, want true")
+		}
+	}
+
+	for _, tc := range []struct {
+		name         string
+		firstParams  ed25519.Parameters
+		secondParams ed25519.Parameters
+		want         bool
+	}{
+		{
+			name:         "tink vs legacy",
+			firstParams:  tinkVariant,
+			secondParams: legacyVariant,
+		},
+		{
+			name:         "tink vs crunchy",
+			firstParams:  tinkVariant,
+			secondParams: crunchyVariant,
+		},
+		{
+			name:         "tink vs no prefix",
+			firstParams:  tinkVariant,
+			secondParams: noPrefixVariant,
+		},
+		{
+			name:         "legacy vs crunchy",
+			firstParams:  legacyVariant,
+			secondParams: crunchyVariant,
+		},
+		{
+			name:         "legacy vs no prefix",
+			firstParams:  legacyVariant,
+			secondParams: noPrefixVariant,
+		},
+		{
+			name:         "crunchy vs no prefix",
+			firstParams:  crunchyVariant,
+			secondParams: noPrefixVariant,
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.firstParams.Equals(&tc.secondParams) {
+				t.Errorf("tc.firstParams.Equals(&tc.secondParams) = true, want false")
+			}
+		})
+	}
+}
