439ee8160830f2a297279245114a49541f1847ef
Juerg Wullschleger <juerg@google.com>
2024-09-25 03:40:55 -0700

    Use a AES-GCM instead of a AES-EAX key in the test vector, which is supported in all languages.
    And also add tests with these test vectors in Golang and the cross language tests.
    
    PiperOrigin-RevId: 678628530
    Change-Id: Id1de1c983e4fa64608970b9d01a8f09fb5786dab

--- keyset/binary_io_test.go
+++ keyset/binary_io_test.go
@@ -16,6 +16,7 @@ package keyset_test
 
 import (
 	"bytes"
+	"encoding/hex"
 	"strings"
 	"testing"
 
@@ -23,6 +24,7 @@ import (
 	"github.com/tink-crypto/tink-go/v2/aead"
 	"github.com/tink-crypto/tink-go/v2/insecurecleartextkeyset"
 	"github.com/tink-crypto/tink-go/v2/keyset"
+	"github.com/tink-crypto/tink-go/v2/mac"
 	"github.com/tink-crypto/tink-go/v2/testkeyset"
 	"github.com/tink-crypto/tink-go/v2/testutil"
 
@@ -73,6 +75,45 @@ func TestBinaryIOEncrypted(t *testing.T) {
 
 	if !proto.Equal(kse1, kse2) {
 		t.Errorf("written encrypted keyset (%s) doesn't match read encrypted keyset (%s)", kse1, kse2)
+	}
+}
+
+func TestReadInBinaryWithTestVector(t *testing.T) {
+	serializedKeysetEncryptionKeyset, err := hex.DecodeString("08cd9bdff30312540a480a30747970652e676f6f676c65617069732e636f6d2f676f6f676c652e63727970746f2e74696e6b2e41657347636d4b657912121a1082bbe6de4bf9a7655305615af46e594c1801100118cd9bdff3032001")
+	if err != nil {
+		t.Fatalf("hex.DecodeString() err = %v, want nil", err)
+	}
+	encryptedKeyset, err := hex.DecodeString("129101013e77cdcd28f57ffb418afa7f25d48a74efe720246e9aa538f33a702888bb7c48bce0e5a016a0c8e9085066d67c7c7fb40dceb176a3a10c7f7ab30c564dd8e2d918a2fc2d2e9a0245c537ff6d1fd756ff9d6de5cf4eb7f229de215e6e892f32fd703d0c9c3d2168813ad5bbc6ce108fcbfed0d9e3b14faae3e3789a891346d983b1ecca082f0546163351339aa142f574")
+	if err != nil {
+		t.Fatalf("hex.DecodeString() err = %v, want nil", err)
+	}
+	associatedData := []byte("associatedData")
+
+	data := []byte("data")
+	tag, err := hex.DecodeString("018f2d72de5055e622591fcf0fb85a7b4158e96f68")
+	if err != nil {
+		t.Fatalf("hex.DecodeString() err = %v, want nil", err)
+	}
+
+	keysetEncryptionHandle, err := testkeyset.Read(keyset.NewBinaryReader(bytes.NewBuffer(serializedKeysetEncryptionKeyset)))
+	if err != nil {
+		t.Fatalf("testkeyset.Read() err = %v, want nil", err)
+	}
+	keysetEncryptionAead, err := aead.New(keysetEncryptionHandle)
+	if err != nil {
+		t.Fatalf("aead.New(keysetEncryptionHandle) err = %v, want nil", err)
+	}
+
+	handle, err := keyset.ReadWithAssociatedData(keyset.NewBinaryReader(bytes.NewBuffer(encryptedKeyset)), keysetEncryptionAead, associatedData)
+	if err != nil {
+		t.Fatalf("keyset.ReadWithAssociatedData() err = %v, want nil", err)
+	}
+	primitive, err := mac.New(handle)
+	if err != nil {
+		t.Fatalf("mac.New(handle) err = %v, want nil", err)
+	}
+	if err := primitive.VerifyMAC(tag, data); err != nil {
+		t.Fatalf("primitive.VerifyMAC(tag, data) err = %v, want nil", err)
 	}
 }
 
