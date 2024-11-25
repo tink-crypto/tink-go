d79bb15af2c989e1e3dc400f431cb74d13c0c9ce
Moreno Ambrosin <ambrosin@google.com>
2024-11-25 04:23:40 -0800

    Add and register full primitive constructor for XChaCha20-Poly1305
    PiperOrigin-RevId: 699932175
    Change-Id: I8bf50098682293e9149e0b1c2ad7a424e24253c5

--- aead/xchacha20poly1305/xchacha20poly1305.go
+++ aead/xchacha20poly1305/xchacha20poly1305.go
@@ -22,6 +22,7 @@ import (
 	"github.com/tink-crypto/tink-go/v2/core/registry"
 	"github.com/tink-crypto/tink-go/v2/internal/internalregistry"
 	"github.com/tink-crypto/tink-go/v2/internal/protoserialization"
+	"github.com/tink-crypto/tink-go/v2/internal/registryconfig"
 )
 
 func init() {
@@ -38,6 +39,9 @@ func init() {
 		panic(fmt.Sprintf("xchacha20poly1305.init() failed: %v", err))
 	}
 	if err := protoserialization.RegisterParametersSerializer[*Parameters](&parametersSerializer{}); err != nil {
+		panic(fmt.Sprintf("xchacha20poly1305.init() failed: %v", err))
+	}
+	if err := registryconfig.RegisterPrimitiveConstructor[*Key](primitiveConstructor); err != nil {
 		panic(fmt.Sprintf("xchacha20poly1305.init() failed: %v", err))
 	}
 }
