9a0b6b8882f79333ac30e6e09c8186c3a1c5338d
Lukas Zobernig <zlukas@google.com>
2025-03-10 06:04:24 -0700

    Add ML-DSA implementation (marshalling).
    PiperOrigin-RevId: 735346917
    Change-Id: I5aff24f6c2d3f878bde01ca821accbeae692cb99

---
+++ internal/signature/mldsa/marshal_test.go
@@ -0,0 +1,163 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package mldsa
+
+import (
+	mathrand "math/rand/v2"
+	"testing"
+)
+
+func randomPolyN(n int) *poly {
+	res := poly{}
+	for i := range res {
+		res[i] = rZq(mathrand.IntN(n))
+	}
+	return &res
+}
+
+func TestPolySimpleBitPackUnpack(t *testing.T) {
+	for i := 0; i < numTestValues; i++ {
+		// Generate random polynomial with random coefficient number of bits [1, qBits].
+		bits := 1 + mathrand.IntN(qBits)
+		exp := randomPolyN(1 << bits)
+		got := simpleBitUnpackPoly(exp.simpleBitPack(bits), bits)
+		if !comparePoly(exp, got) {
+			t.Fatalf("p.encode(%v).decodePoly(...) = %v, want %v", bits, got, exp)
+		}
+	}
+}
+
+func TestPolyBitPackUnpack(t *testing.T) {
+	for i := 0; i < numTestValues; i++ {
+		// Generate random polynomial with random coefficient number of bits [1, qBits].
+		bits := 1 + mathrand.IntN(qBits)
+		a := 1 << (bits - 1)
+		exp := randomPolyN(a)
+		got := bitUnpackPoly(exp.bitPack(rZq(a), bits), rZq(a), bits)
+		if !comparePoly(exp, got) {
+			t.Fatalf("p.encodeSigned(%v, %v).decodePolySigned(...) = %v, want %v", a, bits, got, exp)
+		}
+	}
+}
+
+// Note that numOnes is strictly less than a scalar's length.
+func randomVectorHint(par *params, numOnes int) vector {
+	if numOnes > degree {
+		panic("numOnes must be <= degree")
+	}
+	res := makeZeroVector(par.k)
+	// Set the first numOnes coefficients to 1.
+	for i := 0; i < numOnes; i++ {
+		res[0][i] = rZq(1)
+	}
+	// And then Fisher-Yates shuffle.
+	mathrand.Shuffle(len(res)*len(res[0]), func(i, j int) {
+		k := i / len(res[0])
+		l := i % len(res[0])
+		m := j / len(res[0])
+		n := j % len(res[0])
+		res[k][l], res[m][n] = res[m][n], res[k][l]
+	})
+	return res
+}
+
+func compareVector(u, v vector) bool {
+	if len(u) != len(v) {
+		return false
+	}
+	for i := range u {
+		if !comparePoly(u[i], v[i]) {
+			return false
+		}
+	}
+	return true
+}
+
+func TestVectorHintBitPackUnpack(t *testing.T) {
+	pars := []struct {
+		name string
+		par  *params
+	}{
+		{"MLDSA44", MLDSA44},
+		{"MLDSA65", MLDSA65},
+		{"MLDSA87", MLDSA87},
+	}
+	for _, par := range pars {
+		for i := 0; i < numTestValues; i++ {
+			exp := randomVectorHint(par.par, par.par.omega)
+			got, err := par.par.hintBitUnpackVector(exp.hintBitPack(par.par))
+			if err != nil {
+				t.Errorf("decodeVector(%v) failed: %v", par.name, err)
+			}
+			if !compareVector(exp, got) {
+				t.Errorf("decodeVector(%v) failed", par.name)
+			}
+		}
+	}
+}
+
+func onesVectorHint(par *params, wantNumOnes int) vector {
+	res := makeZeroVector(par.k)
+	numOnes := 0
+	for i := range res {
+		for j := range res[i] {
+			if numOnes < wantNumOnes {
+				res[i][j] = rZq(1)
+				numOnes++
+			} else {
+				res[i][j] = rZq(0)
+			}
+		}
+	}
+	return res
+}
+
+func TestHintBitUnpackOverflowVectorFails(t *testing.T) {
+	pars := []struct {
+		name string
+		par  *params
+	}{
+		{"MLDSA44", MLDSA44},
+		{"MLDSA65", MLDSA65},
+		{"MLDSA87", MLDSA87},
+	}
+	for _, par := range pars {
+		exp := onesVectorHint(par.par, par.par.omega+1)
+		_, err := par.par.hintBitUnpackVector(exp.hintBitPack(par.par))
+		if err == nil {
+			t.Errorf("decodeVector(%v) succeeded, want error", par.name)
+		}
+	}
+}
+
+func TestHintBitUnpackInvalidPaddingVectorFails(t *testing.T) {
+	pars := []struct {
+		name string
+		par  *params
+	}{
+		{"MLDSA44", MLDSA44},
+		{"MLDSA65", MLDSA65},
+		{"MLDSA87", MLDSA87},
+	}
+	for _, par := range pars {
+		exp := onesVectorHint(par.par, 1)
+		enc := exp.hintBitPack(par.par)
+		enc[1] = byte(255)
+		_, err := par.par.hintBitUnpackVector(enc)
+		if err == nil {
+			t.Errorf("decodeVector(%v) succeeded, want error", par.name)
+		}
+	}
+}
--- internal/signature/mldsa/mldsa.go
+++ internal/signature/mldsa/mldsa.go
@@ -15,6 +15,10 @@
 // Package mldsa implements ML-DSA as specified in NIST FIPS 204 (https://doi.org/10.6028/NIST.FIPS.204).
 package mldsa
 
+import (
+	"math/bits"
+)
+
 const (
 	// Base ring modulus.
 	q = 8380417
@@ -30,3 +34,94 @@ const (
 	// Dropped bits.
 	d = 13
 )
+
+type params struct {
+	// ML-DSA parameters (see https://doi.org/10.6028/NIST.FIPS.204).
+	tau        int
+	lambda     int
+	log2Gamma1 int
+	gamma2     uint32
+	k          int
+	l          int
+	eta        int
+	omega      int
+	// Precomputed derived parameters.
+	etaBits int
+	w1Bits  int
+}
+
+type paramsOpts struct {
+	tau        int
+	lambda     int
+	log2Gamma1 int
+	invGamma2  uint32
+	k          int
+	l          int
+	eta        int
+	omega      int
+}
+
+func newParams(par paramsOpts) *params {
+	gamma2 := (q - 1) / par.invGamma2
+	etaBits := bits.Len(uint(2 * par.eta))
+	w1Bits := bits.Len(uint((q-1)/(2*gamma2) - 1))
+	return &params{par.tau, par.lambda, par.log2Gamma1, gamma2, par.k, par.l, par.eta, par.omega, etaBits, w1Bits}
+}
+
+var (
+	// MLDSA44 defines parameters for ML-DSA-44.
+	MLDSA44 = newParams(paramsOpts{
+		tau:        39,
+		lambda:     128,
+		log2Gamma1: 17,
+		invGamma2:  88,
+		k:          4,
+		l:          4,
+		eta:        2,
+		omega:      80,
+	})
+	// MLDSA65 defines parameters for ML-DSA-65.
+	MLDSA65 = newParams(paramsOpts{
+		tau:        49,
+		lambda:     192,
+		log2Gamma1: 19,
+		invGamma2:  32,
+		k:          6,
+		l:          5,
+		eta:        4,
+		omega:      55,
+	})
+	// MLDSA87 defines parameters for ML-DSA-87.
+	MLDSA87 = newParams(paramsOpts{
+		tau:        60,
+		lambda:     256,
+		log2Gamma1: 19,
+		invGamma2:  32,
+		k:          8,
+		l:          7,
+		eta:        2,
+		omega:      75,
+	})
+)
+
+// publicKey represents a ML-DSA public key.
+type publicKey struct {
+	rho [32]byte
+	t1  vector
+	// Cached public key hash.
+	tr [64]byte
+	// Corresponding parameters.
+	par *params
+}
+
+// secretKey represents a ML-DSA secret key.
+type secretKey struct {
+	rho [32]byte
+	kK  [32]byte
+	tr  [64]byte
+	s1  vector
+	s2  vector
+	t0  vector
+	// Corresponding parameters.
+	par *params
+}
