bf77f8e08e14e872715b3c125d70dc3a0053e7ac
Moreno Ambrosin <ambrosin@google.com>
2025-04-10 07:50:55 -0700

    Add `hmacprf.{Key,Parameters}`
    PiperOrigin-RevId: 746028679
    Change-Id: If66be7a1e3be69a0783693ee2bd1691407f8c322

---
+++ prf/hmacprf/key_test.go
@@ -0,0 +1,163 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hmacprf_test
+
+import (
+	"bytes"
+	"fmt"
+	"testing"
+
+	"github.com/tink-crypto/tink-go/v2/insecuresecretdataaccess"
+	"github.com/tink-crypto/tink-go/v2/prf/hmacprf"
+	"github.com/tink-crypto/tink-go/v2/secretdata"
+)
+
+var key128Bits = []byte{
+	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+}
+var key256Bits = []byte{
+	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+	0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10,
+}
+
+func TestNewKey(t *testing.T) {
+	for _, keyBytes := range [][]byte{key128Bits, key256Bits} {
+		for _, hashType := range []hmacprf.HashType{hmacprf.SHA256, hmacprf.SHA512, hmacprf.SHA1, hmacprf.SHA384} {
+			t.Run(fmt.Sprintf("HMAC keySize=%d hashType=%v", len(keyBytes), hashType), func(t *testing.T) {
+				params, err := hmacprf.NewParameters(len(keyBytes), hashType)
+				if err != nil {
+					t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+				}
+				keyBytes := secretdata.NewBytesFromData(keyBytes, insecuresecretdataaccess.Token{})
+				key, err := hmacprf.NewKey(keyBytes, params)
+				if err != nil {
+					t.Errorf("hmacprf.NewKey() err = %v, want nil", err)
+				}
+				if !key.KeyBytes().Equal(keyBytes) {
+					t.Errorf("KeyBytes() = %v, want %v", key.KeyBytes(), keyBytes)
+				}
+				if !key.Parameters().Equal(params) {
+					t.Errorf("Parameters() = %v, want %v", key.Parameters(), params)
+				}
+				id, required := key.IDRequirement()
+				if id != 0 {
+					t.Errorf("IDRequirement() = %v, want 0", id)
+				}
+				if required {
+					t.Errorf("IDRequirement() = %v, want false", required)
+				}
+				if key.OutputPrefix() != nil {
+					t.Errorf("OutputPrefix() = %v, want nil", key.OutputPrefix())
+				}
+
+				key2, err := hmacprf.NewKey(keyBytes, params)
+				if err != nil {
+					t.Errorf("hmacprf.NewKey() err = %v, want nil", err)
+				}
+				if !key.Equal(key2) {
+					t.Errorf("Equal() = false, want true")
+				}
+				if !key2.Equal(key) {
+					t.Errorf("Equal() = false, want true")
+				}
+			})
+		}
+	}
+}
+
+func TestNewKeyFails(t *testing.T) {
+	params, err := hmacprf.NewParameters(32, hmacprf.SHA256)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes := secretdata.NewBytesFromData(key128Bits, insecuresecretdataaccess.Token{})
+	if _, err := hmacprf.NewKey(keyBytes, params); err == nil {
+		t.Errorf("hmacprf.NewKey() err = nil, want error")
+	}
+}
+
+func TestNotEqualIfDifferentKeyBytes(t *testing.T) {
+	params, err := hmacprf.NewParameters(32, hmacprf.SHA256)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes1 := secretdata.NewBytesFromData(key256Bits, insecuresecretdataaccess.Token{})
+	key1, err := hmacprf.NewKey(keyBytes1, params)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+
+	otherKey := bytes.Clone(key256Bits)
+	otherKey[0] ^= 0xff
+
+	keyBytes2 := secretdata.NewBytesFromData(otherKey, insecuresecretdataaccess.Token{})
+	key2, err := hmacprf.NewKey(keyBytes2, params)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+	if key1.Equal(key2) {
+		t.Errorf("Equal() = true, want false")
+	}
+}
+
+func TestNotEqualIfDifferentKeySizes(t *testing.T) {
+	params, err := hmacprf.NewParameters(32, hmacprf.SHA256)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes1 := secretdata.NewBytesFromData(key256Bits, insecuresecretdataaccess.Token{})
+	key1, err := hmacprf.NewKey(keyBytes1, params)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+
+	params2, err := hmacprf.NewParameters(16, hmacprf.SHA256)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes2 := secretdata.NewBytesFromData(key128Bits, insecuresecretdataaccess.Token{})
+	key2, err := hmacprf.NewKey(keyBytes2, params2)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+	if key1.Equal(key2) {
+		t.Errorf("Equal() = true, want false")
+	}
+}
+
+func TestNotEqualIfDifferentParams(t *testing.T) {
+	params, err := hmacprf.NewParameters(32, hmacprf.SHA256)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes1 := secretdata.NewBytesFromData(key256Bits, insecuresecretdataaccess.Token{})
+	key1, err := hmacprf.NewKey(keyBytes1, params)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+
+	params2, err := hmacprf.NewParameters(32, hmacprf.SHA512)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters() err = %v, want nil", err)
+	}
+	keyBytes2 := secretdata.NewBytesFromData(key256Bits, insecuresecretdataaccess.Token{})
+	key2, err := hmacprf.NewKey(keyBytes2, params2)
+	if err != nil {
+		t.Fatalf("hmacprf.NewKey() err = %v, want nil", err)
+	}
+	if key1.Equal(key2) {
+		t.Errorf("Equal() = true, want false")
+	}
+}
---
+++ prf/hmacprf/parameters_test.go
@@ -0,0 +1,113 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package hmacprf_test
+
+import (
+	"testing"
+
+	"github.com/tink-crypto/tink-go/v2/prf/hmacprf"
+)
+
+func TestParametersValid(t *testing.T) {
+	for _, keySize := range []int{16, 32} {
+		for _, hashType := range []hmacprf.HashType{hmacprf.SHA256, hmacprf.SHA512, hmacprf.SHA1, hmacprf.SHA384} {
+			params, err := hmacprf.NewParameters(keySize, hashType)
+			if err != nil {
+				t.Errorf("hmacprf.NewParameters(%d, %v) failed: %v", keySize, hashType, err)
+			}
+			if params.KeySizeInBytes() != keySize {
+				t.Errorf("params.KeySizeInBytes() = %d, want %d", params.KeySizeInBytes(), keySize)
+			}
+			if params.HashType() != hashType {
+				t.Errorf("params.HashType() = %v, want %v", params.HashType(), hashType)
+			}
+
+			params2, err := hmacprf.NewParameters(keySize, hashType)
+			if err != nil {
+				t.Errorf("hmacprf.NewParameters(%d, %v) failed: %v", keySize, hashType, err)
+			}
+			if !params.Equal(params2) {
+				t.Errorf("Equal() returned false, expected true")
+			}
+			if !params2.Equal(params) {
+				t.Errorf("Equal() returned false, expected true")
+			}
+		}
+	}
+}
+
+func TestParametersInvalidValues(t *testing.T) {
+	for _, tc := range []struct {
+		name      string
+		keySize   int
+		hashType  hmacprf.HashType
+		wantError string
+	}{
+		{
+			name:      "key size too small",
+			keySize:   15,
+			hashType:  hmacprf.SHA256,
+			wantError: "keySizeInBytes must be >= 16, got 15",
+		},
+		{
+			name:      "unknown hash type",
+			keySize:   16,
+			hashType:  hmacprf.UnknownHashType,
+			wantError: "hashType must be specified",
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			if _, err := hmacprf.NewParameters(tc.keySize, tc.hashType); err == nil {
+				t.Errorf("hmacprf.NewParameters(%d, %v) succeeded, expected error with message %q", tc.keySize, tc.hashType, tc.wantError)
+			}
+		})
+	}
+}
+
+func mustCreateParameters(t *testing.T, keySize int, hashType hmacprf.HashType) *hmacprf.Parameters {
+	t.Helper()
+	params, err := hmacprf.NewParameters(keySize, hashType)
+	if err != nil {
+		t.Fatalf("hmacprf.NewParameters(%d, %v) failed: %v", keySize, hashType, err)
+	}
+	return params
+}
+
+func TestParametersNotEquals(t *testing.T) {
+
+	for _, tc := range []struct {
+		name    string
+		params1 *hmacprf.Parameters
+		params2 *hmacprf.Parameters
+	}{
+		{
+			name:    "different key size",
+			params1: mustCreateParameters(t, 32, hmacprf.SHA256),
+			params2: mustCreateParameters(t, 16, hmacprf.SHA256),
+		},
+		{
+			name:    "different hash type",
+			params1: mustCreateParameters(t, 32, hmacprf.SHA256),
+			params2: mustCreateParameters(t, 32, hmacprf.SHA512),
+		},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.params1.Equal(tc.params2) {
+				t.Errorf("Equal() returned true, expected false")
+			}
+		})
+	}
+
+}
